f97bbae48d71c8677f0e34be5bbc8372386f2597
Magnus Nyqvist <magnus.nyq@gmail.com>
2018-10-25 12:10:42 +0200

    basic

--- GridDemo/GridDemo/GridDemo/Source/Source.cpp
+++ GridDemo/GridDemo/GridDemo/Source/Source.cpp
@@ -5,6 +5,7 @@
 #include "Timer.h"
 #include <stdlib.h>
 #include "rlutil.h"
+#pragma comment(lib, "user32")
 
 const int BLACK = 0;
 const int BLUE = 1;
@@ -23,6 +24,16 @@ const int LIGHT_PURPLE = 13;
 const int LIGHT_YELLOW = 14;
 const int LIGHT_WHITE = 15;
 
+const char WALL = '#';
+const char DOOR_CLOSED = '=';
+const char DOOR_OPEN = '/';
+const char GRASS = ',';
+const char FLOOR = '.';
+const char WATER = '~';
+
+void cls();
+void setCursorPosition(int x, int y);
+
 struct Vec
 {
 	int x, y, z;
@@ -35,6 +46,14 @@ struct Vec
 		n.z = z + other.z;
 		return n;
 	}
+	bool operator==(const Vec & other) const
+	{
+		return x == other.x && y == other.y && z == other.z;
+	}
+	bool operator!=(const Vec & other) const
+	{
+		return x != other.x || y != other.y || z != other.z;
+	}
 
 };
 struct Drawable
@@ -42,6 +61,7 @@ struct Drawable
 	char spr = ' ';
 	Vec pos = { 0,0,0 };
 	int color = 7;
+	bool NeedRedraw = false;
 };
 struct Map
 {
@@ -60,13 +80,13 @@ struct Map
 		}
 	}
 
-	void Draw(std::vector<Drawable> * drawQueue)
+	void Draw(std::vector<Drawable*> * drawQueue)
 	{
 		for (int i = 0; i < mapSize.x; i++)
 		{
 			for (int k = 0; k < mapSize.y; k++)
 			{
-				drawQueue->push_back(map[k * mapSize.x + i]);
+				drawQueue->push_back(&map[k * mapSize.x + i]);
 			}
 		}
 	}
@@ -83,7 +103,7 @@ struct Map
 };
 struct Render
 {
-	std::vector<Drawable>	drawQueue;
+	std::vector<Drawable*>	drawQueue;
 	Drawable * 				screen = nullptr;
 	Vec						screenSize;
 
@@ -108,29 +128,26 @@ struct Render
 	}
 	void Clear()
 	{
+		for (auto & t : drawQueue)
+			t->NeedRedraw = false;
 		drawQueue.clear();
-		for (int i = 0; i < screenSize.x; i++)
-		{
-			for (int k = 0; k < screenSize.y; k++)
-			{
-				screen[k * screenSize.x + i] = { ' ', i, k, -10 };
-			}
-		}
 	}
-	void Draw(const Drawable & d)
+	void Draw(Drawable * d)
 	{
 		drawQueue.push_back(d);
 	}
-	void Flush(bool needsReDraw = true)
+	void Flush()
 	{
-		if (needsReDraw)
+		static bool FIRST_DRAW = true;
+		
+		for (auto & d : drawQueue)
 		{
-			system("CLS");
-			for (auto & d : drawQueue)
-			{
-				if (screen[d.pos.y * screenSize.x + d.pos.x].pos.z <= d.pos.z)
-					screen[d.pos.y * screenSize.x + d.pos.x] = d;
-			}
+			screen[d->pos.y * screenSize.x + d->pos.x] = *d;
+		}
+		if (FIRST_DRAW)
+		{
+			FIRST_DRAW = FALSE;
+			setCursorPosition(0, 0);
 			for (int i = 0; i < screenSize.y; i++)
 			{
 				for (int k = 0; k < screenSize.x; k++)
@@ -141,45 +158,63 @@ struct Render
 				std::cout << "\n";
 			}
 		}
+		else
+		{
+			for (int i = 0; i < screenSize.y; i++)
+			{
+				for (int k = 0; k < screenSize.x; k++)
+				{
+					Drawable & target = screen[i * screenSize.x + k];
+					if (target.NeedRedraw)
+					{
+						rlutil::setColor(target.color);
+						setCursorPosition(target.pos.x, target.pos.y);
+						std::cout << target.spr;
+					}
+				}
+			}
+		}
+		setCursorPosition(screenSize.x, screenSize.y);
 		Clear();
 	}
 };
 
 void LoadMap(const std::string & path, Map * map);
-
-bool Move(Drawable & c, Map & map);
+Vec Move(Drawable & c, Map & map);
+void Interact(Drawable & c, Map & map, const Vec & dir);
 
 int main()
 {
+	SendMessage(GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
 	rlutil::saveDefaultColor();
 	Drawable c;
 	c.spr = '@';
-	c.pos = { 11, 5, 0 };
 	c.color = RED;
+	c.pos = { 11, 8, 0 };
 	Render machine;
 	Map map;
 	LoadMap("Assets/Map.txt", &map);
 	machine.screenSize.x = map.mapSize.x;
 	machine.screenSize.y = map.mapSize.y;
 	machine.alloc();
-	map.Draw(&machine.drawQueue);
-	machine.Draw(c);
-	machine.Flush();
-
-
-
+	
 	while (!GetAsyncKeyState(VK_ESCAPE))
 	{
-
-
+		bool reDraw = false;
 		//Update
-		bool redraw = Move(c, map);
-
+		Vec dir = Move(c, map);
+		Interact(c, map, dir);
 		//Draw
 		map.Draw(&machine.drawQueue);
-		machine.Draw(c);
-		machine.Flush(redraw);
+		machine.Draw(&c);
+		machine.Flush();
 	}
+
+
+
+
+
+
 
 
 
@@ -202,17 +237,24 @@ void LoadMap(const std::string & path, Map * map)
 
 	map->alloc();
 
-	std::string line;
 	int counter = 0;
+	std::string line;
 	while (std::getline(in, line))
 	{
 		for (int i = 0; i < map->mapSize.x; i++)
 		{
 			int color = 7;
-			if (line[i] == '.')
-				color = LIGHT_GREEN;
-			else if (line[i] == '#')
+			if (line[i] == GRASS)
+				color = GREEN;
+			else if (line[i] == WALL)
 				color = PURPLE;
+			else if (line[i] == FLOOR)
+				color = WHITE;
+			else if (line[i] == DOOR_CLOSED || line[i] == DOOR_OPEN)
+				color = LIGHT_PURPLE;
+			else if (line[i] == WATER)
+				color = AQUA;
+
 			map->map[counter * map->mapSize.x + i].spr = line[i];
 			map->map[counter * map->mapSize.x + i].color = color;
 		}
@@ -222,39 +264,134 @@ void LoadMap(const std::string & path, Map * map)
 	in.close();
 }
 
-bool Move(Drawable & c, Map & map)
+Vec Move(Drawable & c, Map & map)
 {
 	static bool MovedLastFrame = false;
 	static Timer moveTimer(true);
 
-	bool moveThisFrame = true;
-	bool redraw = false;
+	bool moveThisFrame = false;
 
 	Vec moveDir = {0,0,0};
 
 	if (GetAsyncKeyState(VK_UP))
+	{
+		moveThisFrame = true;
 		moveDir.y = -1;
+	}
 	else if (GetAsyncKeyState(VK_DOWN))
+	{
 		moveDir.y = 1;
-	else if (GetAsyncKeyState(VK_RIGHT))
+		moveThisFrame = true;
+	}
+	
+	if (GetAsyncKeyState(VK_RIGHT))
+	{
+		moveThisFrame = true;
 		moveDir.x = 1;
+	}
 	else if (GetAsyncKeyState(VK_LEFT))
+	{
 		moveDir.x = -1;
-	else
-		moveThisFrame = false;
+		moveThisFrame = true;
+	}
 
-	if ((!MovedLastFrame || moveTimer.Peek() > 0.2) && moveThisFrame)
+	if ((!MovedLastFrame || moveTimer.Peek() > 0.1) && moveThisFrame)
 	{
 		moveTimer.Stop();
 		Vec newPos = c.pos + moveDir;
 		newPos.z = 0;
 		if (newPos.x >= 0 && newPos.x < map.mapSize.x && newPos.y >= 0 && newPos.y < map.mapSize.y)
 		{
-			c.pos = newPos;
-			redraw = true;
+			char type = map.map[newPos.y * map.mapSize.x + newPos.x].spr;
+			if (type != WALL && type != DOOR_CLOSED && type != WATER)
+			{
+				map.map[c.pos.y * map.mapSize.x + c.pos.x].NeedRedraw = true;
+				c.pos = newPos;
+				c.NeedRedraw = true;
+			}
 		}
 	}
 
 	MovedLastFrame = moveThisFrame;
-	return redraw;
+	return moveDir;
+}
+
+void Interact(Drawable & c, Map & map, const Vec & dir)
+{
+	static const Vec EMPTY = { 0,0,0 };
+	static Vec lastDir = { 0,0,0 };
+	static bool InteractedLastFrame = false;
+	bool interactThisFrame = false;
+
+	if (dir != EMPTY)
+		lastDir = dir;
+
+	if (GetAsyncKeyState('E'))
+		interactThisFrame = true;
+
+	if (!InteractedLastFrame && interactThisFrame && lastDir != EMPTY)
+	{
+		Vec InterActAt = c.pos + lastDir;
+		if (InterActAt.x >= 0 && InterActAt.x < map.mapSize.x && InterActAt.y >= 0 && InterActAt.y < map.mapSize.y)
+		{
+			char type = map.map[InterActAt.y * map.mapSize.x + InterActAt.x].spr;
+			if (type == DOOR_CLOSED)
+			{
+				map.map[InterActAt.y * map.mapSize.x + InterActAt.x].spr = DOOR_OPEN;
+				map.map[InterActAt.y * map.mapSize.x + InterActAt.x].NeedRedraw = true;
+			}
+			else if (type == DOOR_OPEN)
+			{
+				map.map[InterActAt.y * map.mapSize.x + InterActAt.x].spr = DOOR_CLOSED;
+				map.map[InterActAt.y * map.mapSize.x + InterActAt.x].NeedRedraw = true;
+			}
+		}
+	}
+	InteractedLastFrame = interactThisFrame;
+}
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+
+void cls()
+{
+	// Get the Win32 handle representing standard output.
+// This generally only has to be done once, so we make it static.
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	COORD topLeft = { 0, 0 };
+
+	// std::cout uses a buffer to batch writes to the underlying console.
+	// We need to flush that to the console because we're circumventing
+	// std::cout entirely; after we clear the console, we don't want
+	// stale buffered text to randomly be written out.
+	std::cout.flush();
+
+	// Figure out the current width and height of the console window
+	if (!GetConsoleScreenBufferInfo(hOut, &csbi)) {
+		// TODO: Handle failure!
+		abort();
+	}
+	DWORD length = csbi.dwSize.X * csbi.dwSize.Y;
+
+	DWORD written;
+
+	// Flood-fill the console with spaces to clear it
+	FillConsoleOutputCharacter(hOut, TEXT(' '), length, topLeft, &written);
+
+	// Reset the attributes of every character to the default.
+	// This clears all background colour formatting, if any.
+	FillConsoleOutputAttribute(hOut, csbi.wAttributes, length, topLeft, &written);
+
+	// Move the cursor back to the top left for the next sequence of writes
+	SetConsoleCursorPosition(hOut, topLeft);
+}
+
+void setCursorPosition(int x, int y)
+{
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	std::cout.flush();
+	COORD coord = { (SHORT)x, (SHORT)y };
+	SetConsoleCursorPosition(hOut, coord);
 }
